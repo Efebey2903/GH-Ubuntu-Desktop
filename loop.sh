7016c6df26518b9ea575088ee4c726d83fe20dbd
Efebey2903 <swatibaljeet0101@gmail.com>
2024-08-12 20:00:21 +0300

    Update loop.sh

--- loop.sh
+++ loop.sh
@@ -10,9 +10,10 @@ websockify --web /usr/share/novnc/ --wrap-mode=ignore 6080 localhost:5900
 echo "Done! Port 6080 and 5900 should now be forwarded. Simply open https://localhost:6080 to access the NoVNC client."
              
 
-# Sonsuz döngü
-while true
-do
-    echo Running i guess
-    sleep 100
+
+while true; do
+    ping -c 1 google.com 
+    curl google.com
+    sleep 120
 done
+
